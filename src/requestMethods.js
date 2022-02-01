ad24f4c79d3f5a2d45bc43e36cb0b89611d64884
tarik lahlali <tarik@tariks-MacBook-Pro.local>
2022-02-01 22:00:39 +0100

    api

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:5000/api/";
-const BASE_URL = "https://laboutique-node.herokuapp.com/api/";
+const BASE_URL = "https://laboutique-node.herokuapp.com/";
 // const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTU5NGM2NzM0NDMyNzdmNThjMjQ2NCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MjgwMDkwNCwiZXhwIjoxNjQzMDYwMTA0fQ.izlIcQfMDTMCIEmtNHGcVGT0rqiZJSYbFIdueSv-Oe4";
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
