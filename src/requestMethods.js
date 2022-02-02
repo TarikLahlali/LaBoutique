5d7b310a74d6d4aa27f3df68e5a5911fbced60c7
tarik lahlali <tarik@tariks-MacBook-Pro.local>
2022-02-02 21:04:06 +0100

    restoring old source code

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-// const BASE_URL = "https://laboutique-node.herokuapp.com/api";
 // const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTU5NGM2NzM0NDMyNzdmNThjMjQ2NCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MjgwMDkwNCwiZXhwIjoxNjQzMDYwMTA0fQ.izlIcQfMDTMCIEmtNHGcVGT0rqiZJSYbFIdueSv-Oe4";
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
